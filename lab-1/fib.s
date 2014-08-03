01167ec2c4dfcf1ccb67e66cb7e879e1572fd38f
stu9458 <stu9458@gmail.com>
2014-08-03 16:40:37 +0800

    The Lab1 is done.Please check.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -1,3 +1,17 @@
+/******************************
+2014/07/31 2014-Embedded homework.
+The Code use 2 register be local value.
+R0 = Is "x" of C-code, be total num and index(input number)
+R4 = use to store index(input num) fo R0
+R5 = Store R0's value and add to R0 when R0 return value.
+LR = R14. Link Register used to save the PC when entering a subroutine.
+
+Push the old value and compute new state.
+If x=0 or x=1 that will pop and compute.
+Finall return to C-Code to user.
+
+By Emp 2014/08/01
+********************************/
 	.syntax unified
 	.arch armv7-a
 	.text
@@ -11,33 +25,38 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, lr} //The function use 2 values. LR is R14
+	
+	mov r4,r0 //R0 is input and sum. Save R4 to R0
+	cmp r4,#0  //if(R4==0)
+	ble .L3	  //Jump to .L3
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	cmp r4,#1  //if(R4==1)
+	ble .L4	   //Jump to .L4
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+        //R4 were store the input number. Let R0 be sum number.
+        //return fib(x-1)
+	subs r0,r4,#1
+	bl fibonacci 
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	mov r5,r0 //The R5 were store R0 number.
+	sub r0,r4,#2
+	bl fibonacci  
 
 	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	add r0,r0,r5 //return fib(x-1)+fib(x-2)
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
+	//if(x==0)return 0
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, pc}		@ EPILOG
 
 .L4:
+	//if(x==1)return 1
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
