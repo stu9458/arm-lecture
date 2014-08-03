51c77b7e03cfb90a88d84ce942069ae8dc005523
stu9458 <stu9458@gmail.com>
2014-08-04 07:32:22 +0800

    Add some commit in 2014/08/04 7:32am

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -29,20 +29,20 @@ fibonacci:
 	
 	mov r4,r0 //R0 is input and sum. Save R4 to R0
 	cmp r4,#0  //if(R4==0)
-	ble .L3	  //Jump to .L3
+	beq .L3	  //Jump to .L3. The change is for "beq(==)" more suit than "ble(<=)" in significance at c-code 
 
-	cmp r4,#1  //if(R4==1)
-	bqe .L4	   //Jump to .L4
+	cmp r4,#1  //if(R4==1). 
+	beq .L4	   //Jump to .L4. The change is for "beq(==)" more suit than "ble(<=)" in significance at c-code 
 
         //R4 were store the input number. Let R0 be sum number.
         //return fib(x-1)
 	subs r0,r4,#1
-	bl fibonacci 
+	bl fibonacci   //if use "b",that just jump.
 
 	mov r5,r0 //The R5 were store R0 number.
-	sub r0,r4,#2
-	bl fibonacci  
-
+	sub r0,r4,#2  //Use r4 to store input-number into r0 and return(that's x-2).
+	bl fibonacci   
+	
 	@ R0 = R5 + R0 (update flags)
 	add r0,r0,r5 //return fib(x-1)+fib(x-2)
 	pop {r4, r5, pc}		@EPILOG
