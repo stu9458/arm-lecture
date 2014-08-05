3db4ed5bb0b74d81f6c73c234e4ff8b0a25976ea
stu9458 <stu9458@gmail.com>
2014-08-05 13:53:45 +0800

    add protect for input number by user.
    2014/08/05
    Add protect conditions to judge input number.The fibonacci be solve by math formula.
    So we know how to limit user to input wrong number into our program.
    EX:pow(((1+(pow(5,1/2)))/2,n).  The (1+pow(5,1/2))/2 = 1.618.  pow(1.618,23)=64048.
    Refer http://en.wikipedia.org/wiki/Fibonacci

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,3 +1,11 @@
+/*
+2014/08/05 add comment
+The fibonacci be solved by math.
+Use the math can compute a number for protect the error of int type(65536).
+pow(((1+(pow(5,1/2)))/2,n)  that's bigest in the formula.
+If you need to know,please the hackpad.
+https://emp-learn.hackpad.com/
+*/
 #include <stdio.h>
 
 extern int fibonacci(int x);
@@ -7,9 +15,12 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
-  printf("Please input a number:");
+  printf("Please input a number(0~23):");//bigest in formala
   scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+   if(number>23)
+	printf("Error!!");
+   else{
+	result = fibonacci(number);   
+  	printf("The fibonacci sequence at %d is: %d\n", number, result);}
+  return 0;
 }
-
